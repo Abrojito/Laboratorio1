e9cc8d85e9c5dbdbd7a583d3b0d974511cfe40bc
abril <abrilbreva@gmail.comgit config --global user.email abrilbrieva@gmail.com>
2025-04-16 12:17:53 -0300

    logo

--- Front(era)/src/pages/WelcomePage.tsx
+++ Front(era)/src/pages/WelcomePage.tsx
@@ -1,13 +1,14 @@
-import React, { } from 'react';
-import { useNavigate} from 'react-router-dom';
+import React, {} from 'react';
+import {useNavigate} from 'react-router-dom';
 import '../styles/WelcomePage.css';
 
 const WelcomePage: React.FC = () => {
     const navigate = useNavigate();
 
-    return(
+    return (
         <>
             <main className="welcome-content">
+                <img src="/dishly.jpg" alt="Logo"/>
                 <h1>Dishly</h1>
                 <p>Tu aplicación de recetas y platos favoritos.</p>
                 <div className="button-group">
@@ -15,7 +16,7 @@ const WelcomePage: React.FC = () => {
                     <button onClick={() => navigate('/signup')}>Signup</button>
                 </div>
             </main>
-         </>
+        </>
     );
 }
 
